4c579768f9606adaa8352c34a1275614c92eb623
dianahope baraka <dianahopebaraka@gmail.com>
2022-10-18 06:28:07 -0700

    Createded header file main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef main.h
+#define main.h
+
+/**
+ *
+ * File name: main.h
+ * It contains prototypes of my functions
+ *
+ */
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+
+
+
+
+
+
+
+#endif
