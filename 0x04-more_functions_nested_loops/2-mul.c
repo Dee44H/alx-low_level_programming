77e71b561450ffad376b69fca2cccd9115a53aa4
dianahope baraka <dianahopebaraka@gmail.com>
2022-10-23 07:09:11 -0700

    Created file 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+*mul - Multiplies two integers 'a'and 'b'
+*@a: First integer
+*@b: Second integer
+*
+*Return: result of multiplication of 'a'and 'b'
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
