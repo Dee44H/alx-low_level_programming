a4fd03376d95d8d9d24424876b559e9992e6bd2b
dianahope baraka <dianahopebaraka@gmail.com>
2022-10-23 06:41:51 -0700

    Created file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: digit to be checked.
+ *
+ * Return: 1 if c is a digit and 0 if otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
