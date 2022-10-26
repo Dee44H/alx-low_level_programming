01aaf50ac0589a7da50d0ca3f76468df0cd877d8
dianahope baraka <dianahopebaraka@gmail.com>
2022-10-26 11:55:06 -0700

    Created file 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the second half of the string
+ *@str: string to be examined
+ *
+ *
+ * return: void
+ */
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+
+	_putchar('\n');
+}
