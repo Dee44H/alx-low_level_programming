257dba980f2e46c21813795a42f3e801510a2a50
dianahope baraka <dianahopebaraka@gmail.com>
2022-10-26 06:29:00 -0700

    Created file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
+
+
