e5509486374e1d4a8f4d3c6f12e20825a64e3169
Dee44H <dianahopebaraka@gmail.com>
2022-11-22 14:43:54 +0000

    Created file 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: String to be printed
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+
+	_putchar('\n');
+
+}
